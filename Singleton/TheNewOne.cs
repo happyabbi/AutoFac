5a18ece61d94ec8f97e06069cb92ad8f17593d51
Abraham Chen <happyabbi@gmail.com>
2017-04-04 15:54:25 +0800

    autofac singleton test

---
+++ Singleton/TheNewOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+	public class TheNewOne
+	{
+		private Guid UniqueKey = Guid.NewGuid();
+
+		public TheNewOne()
+		{
+			Thread.Sleep(2000);
+			Console.WriteLine("Constructor Executed");
+		}
+
+		public void ShowUniqueKey()
+		{
+			Console.WriteLine("Unique Key={0}", UniqueKey);
+		}
+	}
+}
